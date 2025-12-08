489c4936e38e19e5411659bc9f94730ec09ea7d6
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-12-08 10:02:15 +0100

    app module changed

--- randomNameGenerator.Frontend/src/app/app.module.ts
+++ randomNameGenerator.Frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,12 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [provideHttpClient(), {
+    provide: APP_INITIALIZER,
+    useFactory: (cfg: ConfigService)=> () =>cfg.load(),
+    deps: [ConfigService],
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
